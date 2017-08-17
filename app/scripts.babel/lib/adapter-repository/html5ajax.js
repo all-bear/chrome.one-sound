2227458aacf49aa2e5975b7e3617e553d9ff9210
allbear <ol.birjukov@gmail.com>
2017-08-17 13:05:59 +0300

    Improve html5 ajax adapter to better handling page reloading

--- app/scripts.babel/lib/adapter-repository/html5ajax.js
+++ app/scripts.babel/lib/adapter-repository/html5ajax.js
@@ -1,44 +1,86 @@
 import {Adapter} from '../adapter';
 import {Html5AdapterRepository, Html5AdapterBehaviour} from './html5';
 
-const RENDER_PAGE_TIMEOUT = 500;
-const CHANGE_URL_TIMEOUT = 100;
-const HANDLE_PAGE_LOAD_ATTEMPTS = 15;
+const NEW_ELEMENTS_FIND_INTERVAL = 1000;
+const LOCATION_CHANGE_INTERVAL = 500;
+const NEW_ELEMENTS_FIND_ATTEMPT = 30;
 
 const TYPE = 'html5ajax';
 export class Html5AjaxAdapterRepository extends Html5AdapterRepository {
-  loadOn(cb) {
-    super.loadOn(cb);
+  getAdapter(element) {
+    return new Adapter({
+      type: TYPE,
+      behavior: new Html5AdapterBehaviour(element)
+    });
+  }
 
-    let prevHref = window.location.href;
+  onLoad(repositoryAdapter) {
+    this.trackedElements = [];
+    this.trackedAdapters = [];
+
+    const findAdapters = () => {
+      let prevElementsCount = 0;
+      let attempts = NEW_ELEMENTS_FIND_ATTEMPT;
+
+      const interval = setInterval(() => {
+        this.trackedAdapters.forEach((adapter) => adapter.onChange());
+        
+        if (!(attempts--)) {
+          clearInterval(interval);
+          return;
+        }
+
+        const elements = this.playerDomElements;
+
+        if (elements.length == prevElementsCount) {
+          return;
+        }
+
+        if (elements.length <= prevElementsCount) {
+          repositoryAdapter.redraw();
+          return;
+        }
+
+        const newElements = elements.slice(-(elements.length - prevElementsCount));
+
+        newElements.forEach((element) => {
+          const isAlreadyUnderTracking = this.trackedElements.find((savedElement) => {return savedElement === element});
+
+          if (isAlreadyUnderTracking) {
+            return;
+          }
+
+          const adapter = this.getAdapter(element);
+          repositoryAdapter.process(this, adapter);
+
+          this.trackedElements.push(element);
+          this.trackedAdapters.push(adapter);
+        });
+
+        prevElementsCount += newElements.length;
+      }, NEW_ELEMENTS_FIND_INTERVAL);
+    }
+
+    let prevHref = '';
     setInterval(() => {
       if (prevHref === window.location.href) {
         return;
       }
 
       prevHref = window.location.href;
+      
+      this.trackedElements = [];
 
-      let attemptsLeft = HANDLE_PAGE_LOAD_ATTEMPTS;
+      repositoryAdapter.destroy(this);
+      repositoryAdapter.redraw();
 
-      const attemptsInterval = setInterval(() => {
-        cb();
-
-        if (!attemptsLeft--) {
-          clearInterval(attemptsInterval);
-        };
-      }, RENDER_PAGE_TIMEOUT);
-      setTimeout(cb, RENDER_PAGE_TIMEOUT);
-    }, CHANGE_URL_TIMEOUT);
+      findAdapters();
+    }, LOCATION_CHANGE_INTERVAL);
   }
 
   get adapters() {
     return new Promise((resolve, reject) => {
-      resolve(this.playerDomElements.map(el => {
-        return new Adapter({
-          type: TYPE,
-          behavior: new Html5AdapterBehaviour(el)
-        });
-      }));
+      resolve([]);
     });
   }
 }
